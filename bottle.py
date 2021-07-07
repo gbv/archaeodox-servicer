efd7d4ac190806e2a70dba97e110228a85cf498b
chris-jan-trapp <christian.trapp@gbv.de>
2021-07-07 08:44:59 +0200

    In it.

---
+++ bottle.py
@@ -0,0 +1,25 @@
+from flask import Flask
+import requests
+
+app = Flask(__name__)
+
+
+
+@app.route('/', defaults={'n':10})
+@app.route('/<int:n>')
+def root(n):
+    params = {'num': n,
+              'min': 1,
+              'max': 20,
+              'base': '10',
+              'md': 'new',
+              'format': 'plain',
+              'col': 1}
+    headers = {'user-agent': 'christian.trapp@gbv.de'}
+    r = requests.get("https://random.org/integers/",
+                     params=params,
+                     headers=headers)
+    if r.status_code == 200:
+        return r.text
+    else:
+        return r.url, 'failed'
