eb94f6e3faff6b9ff1df5ba48d33a22ff5492eb3
chris-jan-trapp <christian.trapp@gbv.de>
2021-07-13 16:08:13 +0200

    Added auth step to ed client ctor.

--- app/easydb_client.py
+++ app/easydb_client.py
@@ -1,19 +1,38 @@
 import json, requests
 from os.path import join as path_join
+import credentials
 
 
 class EasydbClient:
     API_PATH = "api/v1"
 
-    def __init__(self, url, session_token):
+    def __init__(self, url):
         self.url = url
-        self.token = session_token
         self.session_url = path_join(url,
                                      EasydbClient.API_PATH,
                                      "session")
         self.search_url = path_join(url,
                                     EasydbClient.API_PATH,
                                     "search")
+        self.session_auth_url = path_join(url,
+                                          EasydbClient.API_PATH,
+                                          "session",
+                                          "authenticate")
+        self.acquire_session()
+
+    def acquire_session(self):
+        session_response = requests.get(self.session_url)
+        if session_response.status_code == 200:
+            self.session_token = session_response.content
+            jason = {"token": self.session_token,
+                     "login": credentials.USER_NAME,
+                     "password": credentials.PASSWORD}
+            auth_response = requests.post(self.session_auth_url, json=jason)
+            if not auth_response.status_code == 200:
+                raise ValueError(f"Failed to authenticate: {auth_response.content}")
+        else:
+            raise ValueError(f"Failed to acquire session from {self.session_url}")
+
 
     def get_item(self, item_type, id, id_field="_id", pretty=0):
         search = {"type": "in",
