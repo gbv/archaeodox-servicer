c86f0d426e6705b16a74f39ed54e916135fe3009
chris-jan-trapp <christian.trapp@gbv.de>
2021-08-11 15:57:21 +0200

    Added hekate workspace.

--- app/settings.py
+++ app/settings.py
@@ -1,5 +1,5 @@
 OBJECT_TYPE = "teller"
-GEO_SERVER_URL = "https://geodienste.gbv.de/nld/wfs"
+GEO_SERVER_URL = "https://geodienste.gbv.de/nld/hekate/wfs"
 GEOMETRY = "found_at"
 ATTRIBUTES = ["text", ]
 
@@ -8,13 +8,13 @@ CONVERSION_URL = "http://converter:5000/gml/"
 
 TRANSACTION_ATTRIBUTES = {"version": "1.1.0",
                           "service": "WFS",
-                          "xmlns": "http://esx-170.gbv.de:8080/geoserver/gbv",
+                          "xmlns": "http://geodienste.gbv.de/nld/hekate/",
                           "xmlns:gbv": "gbv",
                           "xmlns:gml": "http://www.opengis.net/gml",
                           "xmlns:ogc": "http://www.opengis.net/ogc",
                           "xmlns:wfs": "http://www.opengis.net/wfs",
                           "xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
-                          "xsi:schemaLocation": """http://esx-170.gbv.de:8080/geoserver/
+                          "xsi:schemaLocation": """http://geodienste.gbv.de/nld/hekate/
                                                    http://www.opengis.net/wfs
                                                    ../wfs/1.1.0/WFS.xsd"""
                           }
