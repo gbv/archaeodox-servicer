ffa842d2016a88d016cb680526b181c73d069902
chris-jan-trapp <christian.trapp@gbv.de>
2022-11-10 11:56:10 +0100

    Removed dante client, moved couch client, added couch creation capability to DbCreatingHandler.

--- app/dante_field/d_test.py
+++
@@ -1,7 +0,0 @@
-from dante import DanteTreeNode
-import pickle
-
-with open('amh_datierung.pkl', 'rb') as date_file:
-    datierungen = pickle.load(date_file)
-
-print(datierungen.flatten(3))
--- app/edbHandler.py
+++ app/edbHandler.py
@@ -1,3 +1,14 @@
+import os
+from .couch import Client as CouchClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
+COUCH_HOST = "http://esx-80.gbv.de:5984"
+COUCHDB_ADMIN_USER = os.getenv('COUCHDB_ADMIN_USER')
+COUCHDB_ADMIN_PASSWORD = os.getenv('COUCHDB_ADMIN_PASSWORD')
+
+
 class EdbHandler:
     def __init__(self, incoming_request, logger):
         self.full_data = incoming_request.get_json()
@@ -14,7 +25,20 @@ class EdbHandler:
 class DbCreatingHandler(EdbHandler):
     def process_request(self):
         self.logger.debug(f'Handling {self.inner_data}')
-        
+        couch = CouchClient(COUCH_HOST, auth_from_env=True)
         database = self.object_data
-        database['password'] = 'geheim'
+        database_name = database['db_name']
+
+        if not database['password']:
+            self.logger.info(f"No credentials for {database_name} found, rectifying this.")
+            if couch.has_database(database_name=database_name):
+                self.logger.info("Creaitng database and user.")
+                user = couch.create_db_user(database_name, database_name)
+                couch.add_user_to_db(user['name'], database_name)  
+            else:
+                self.logger.info("Creaitng user.")
+                user = couch.create_db_and_user(database_name, database_name)
+            
+            database['password'] = user['password']
+
         return self.full_data
