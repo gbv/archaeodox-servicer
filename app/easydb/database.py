86e56f0a3b0875824d054f33753c7174983a22b6
tkleinke <thomas.kleinke@gbv.de>
2023-02-13 15:33:27 +0100

    Use single quotes in EasyDB class

--- app/easydb/database.py
+++ app/easydb/database.py
@@ -6,26 +6,26 @@ from app import credentials
     
 
 class EasyDB:
-    API_PATH = "api/v1"
+    API_PATH = 'api/v1'
 
     def __init__(self, url, logger):
         self.url = url
         self.session_url = join(url,
                                 EasyDB.API_PATH,
-                                "session")
+                                'session')
         self.search_url = join(url,
                                EasyDB.API_PATH,
-                               "search")
+                               'search')
         self.session_auth_url = join(url,
                                      EasyDB.API_PATH,
-                                     "session",
-                                     "authenticate")
+                                     'session',
+                                     'authenticate')
         self.db_url = join(url,
                            EasyDB.API_PATH,
-                           "db")
+                           'db')
         self.objects_url = join(url,
                                 EasyDB.API_PATH,
-                                "objects")
+                                'objects')
         self.logger = logger
 
     def acquire_session(self):
@@ -34,25 +34,25 @@ class EasyDB:
             session_info = json.loads(session_response.content)
             self.session_token = session_info['token']
 
-            params = {"token": self.session_token,
-                      "login": credentials.EASYDB_USER_NAME,
-                      "password": credentials.EASYDB_PASSWORD}
+            params = {'token': self.session_token,
+                      'login': credentials.EASYDB_USER_NAME,
+                      'password': credentials.EASYDB_PASSWORD}
             auth_response = requests.post(self.session_auth_url,
                                           params=params)
-            self.logger.debug(f"Attempting auth.")
+            self.logger.debug(f'Attempting auth.')
             if not auth_response.status_code == 200:
-                raise ValueError(f"Failed to authenticate: {auth_response.text}")
+                raise ValueError(f'Failed to authenticate: {auth_response.text}')
         else:
-            raise ValueError(f"Failed to acquire session from {self.session_url}")
+            raise ValueError(f'Failed to acquire session from {self.session_url}')
 
-    def get_item(self, item_type, field_value, field_name="_id", pretty=0, token=None):
-        search = {"type": "in",
-                   "bool": "must",
-                   "fields": [".".join((item_type, field_name))],
-                   "in": [field_value]
+    def get_item(self, item_type, field_value, field_name='_id', pretty=0, token=None):
+        search = {'type': 'in',
+                   'bool': 'must',
+                   'fields': ['.'.join((item_type, field_name))],
+                   'in': [field_value]
                    }
         token = token if token is not None else self.session_token
-        params = {"token": token}
+        params = {'token': token}
 
         response = requests.post(self.search_url, params=params, json={ 'search': [search] })
 
@@ -64,13 +64,13 @@ class EasyDB:
     def get_tags(self, token=None):
         tags_url = join(self.url,
                         EasyDB.API_PATH,
-                        "tags")
-        params = {"token": token if token is not None else self.session_token}
+                        'tags')
+        params = {'token': token if token is not None else self.session_token}
         return requests.get(tags_url, params=params).json()
 
     def get_by_global_id(self, id, version='latest', token=None):
         get_url = f'{self.objects_url}/id/{id}/{version}'
-        params = {"token": token if token is not None else self.session_token}
+        params = {'token': token if token is not None else self.session_token}
         params['format'] = 'long'
 
         response = requests.get(get_url, params=params)
@@ -84,7 +84,7 @@ class EasyDB:
         get_url = join(self.db_url,
                        f'{item_type}/{item_type}__all_fields/',
                        str(id))
-        params = {"token": token if token is not None else self.session_token}
+        params = {'token': token if token is not None else self.session_token}
         params['format'] = 'long'
 
         response = requests.get(get_url, params=params)
@@ -103,14 +103,14 @@ class EasyDB:
             latest[item_type][k] = v
         update_url = join(self.db_url,
                           item_type)
-        params = {"token": token if token is not None else self.session_token}
+        params = {'token': token if token is not None else self.session_token}
         response = requests.post(update_url, json=[latest], params=params)
         if not response.ok:
             raise ConnectionError(response.text)
         return response.ok
 
     def create_object(self, object_type, fields_data, token=None):
-        params = {"token": token if token is not None else self.session_token}
+        params = {'token': token if token is not None else self.session_token}
         data = { '_mask': 'field_datenbank_anlage' }
         fields_data['_version'] = 1
         data[object_type] = fields_data
@@ -138,9 +138,9 @@ class EasyDB:
             file_name = file_information['original_filename']
             mime_type = mimetypes.guess_type(file_name)[0]
             files.append({
-                "name": file_name,
-                "url": dp.get(file_information, 'versions/original/download_url'),
-                "mime_type": mime_type
+                'name': file_name,
+                'url': dp.get(file_information, 'versions/original/download_url'),
+                'mime_type': mime_type
             })
         
         return files
