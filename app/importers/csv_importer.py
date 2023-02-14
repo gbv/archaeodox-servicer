52940cea4ff205796a440e457ab074e445bac09e
tkleinke <thomas.kleinke@gbv.de>
2023-02-14 18:22:16 +0100

    Fix __get_category for project specific categories

--- app/importers/csv_importer.py
+++ app/importers/csv_importer.py
@@ -19,7 +19,7 @@ def run(file_data, file_name, field_database):
 def __get_category(file_name):
     extracted_category = __extract_category_from_file_name(file_name)
     for category in settings.CSVImporter.ALLOWED_CATEGORIES:
-        if extracted_category.lower() == category.lower():
+        if extracted_category.replace('+', ':').lower() == category.lower():
             return category
     raise ValueError(f'Failed to import file: {file_name}. Category {extracted_category} is not a valid category.')
 
