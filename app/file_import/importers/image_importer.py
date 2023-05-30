7d16c58e0567ac6992eda2e3b2ae7f993a7d29fa
tkleinke <thomas.kleinke@gbv.de>
2023-05-30 11:05:44 +0200

    Set category to PlanDrawing and set depicts & map layer relations if image file contains a profile/planum identifier

--- app/file_import/importers/image_importer.py
+++ app/file_import/importers/image_importer.py
@@ -1,4 +1,4 @@
-import mimetypes, io
+import mimetypes, io, re
 from os.path import basename
 from PIL import Image
 from geotiff import GeoTiff
@@ -6,15 +6,16 @@ from geotiff import GeoTiff
 from app import settings, messages
 
 
-def run(image_data, image_name, field_database):
-    image_document = field_database.get_or_create_document(image_name)
+def run(image_data, image_file_name, field_database):
+    image_document = field_database.get_or_create_document(image_file_name)
     id = image_document['_id']
+    planum_or_profile_identifier = __get_planum_or_profile_identifier(image_file_name)
 
     image_object = io.BytesIO(image_data)
     image = Image.open(io.BytesIO(image_object.getvalue()))
-    __initialize_image_document(image_document, image_name, image)
+    __initialize_image_document(image_document, image_file_name, planum_or_profile_identifier, image)
 
-    mimetype, encoding = mimetypes.guess_type(image_name)
+    mimetype, encoding = mimetypes.guess_type(image_file_name)
     if mimetype is None:
         return
     if mimetype == 'image/tiff':
@@ -28,7 +29,19 @@ def run(image_data, image_name, field_database):
         raise ConnectionError(response.content)
     field_database.update_document(id, image_document)
 
-def __initialize_image_document(image_document, image_file_name, image):
+    if planum_or_profile_identifier is not None:
+        __set_relations(image_document, planum_or_profile_identifier, field_database)
+
+def __get_planum_or_profile_identifier(image_file_name):
+    planum_prefix = settings.FileImport.CATEGORY_PREFIXES['Planum']
+    profile_prefix = settings.FileImport.CATEGORY_PREFIXES['Profile']
+    match = re.search(r'(' + planum_prefix + '|' + profile_prefix + ')\d+', image_file_name)
+    if match is not None:
+        return match[0]
+    else:
+        return None
+
+def __initialize_image_document(image_document, image_file_name, planum_or_profile_identifier, image):
     width, height = image.size
     resource = image_document['resource']
     resource['width'] = width
@@ -36,6 +49,43 @@ def __initialize_image_document(image_document, image_file_name, image):
     resource['originalFilename'] = image_file_name
     if 'relations' not in resource:
         resource['relations'] = {}
+    if planum_or_profile_identifier is not None:
+        resource['category'] = 'PlanDrawing'
+
+def __set_relations(image_document, planum_or_profile_identifier, field_database):
+    planum_or_profile_document = field_database.get_or_create_document(planum_or_profile_identifier)
+    project_document = field_database.get_or_create_document(field_database.name)
+    __link_with_profile_or_planum(image_document, planum_or_profile_document)
+    __set_as_map_layer(image_document, project_document)
+    field_database.update_document(planum_or_profile_document['resource']['id'], planum_or_profile_document)
+    field_database.update_document('project', project_document)
+    field_database.update_document(image_document['resource']['id'], image_document)
+
+def __link_with_profile_or_planum(image_document, planum_or_profile_document):
+    __set_depicted_in_relation(image_document, planum_or_profile_document)
+    __set_depicts_relation(image_document, planum_or_profile_document)
+
+def __set_as_map_layer(image_document, project_document):
+    __set_has_map_layer_relation(image_document, project_document)
+    __set_is_map_layer_relation(image_document)
+
+def __set_depicted_in_relation(image_document, planum_or_profile_document):
+    resource = planum_or_profile_document['resource']
+    if 'relations' not in resource:
+        resource['relations'] = {}
+    resource['relations']['isDepictedIn'] = [image_document['resource']['id']]
+
+def __set_depicts_relation(image_document, planum_or_profile_document):
+    image_document['resource']['relations']['depicts'] = [planum_or_profile_document['resource']['id']]
+
+def __set_has_map_layer_relation(image_document, project_document):
+    resource = project_document['resource']
+    if 'relations' not in resource:
+        resource['relations'] = {}
+    resource['relations']['hasMapLayer'] = [image_document['resource']['id']]
+
+def __set_is_map_layer_relation(image_document):
+    image_document['resource']['relations']['isMapLayerOf'] = ['project']
 
 def __get_image_bytes(pil_image_object, format, quality=None):
     out_bytes = io.BytesIO()
