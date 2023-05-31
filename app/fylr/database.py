65bc8b445d9263cfc1172221b2c62d778f42fdbd
tkleinke <thomas.kleinke@gbv.de>
2023-05-31 15:16:19 +0200

    Use servicer specific mask for Fylr database operations

--- app/fylr/database.py
+++ app/fylr/database.py
@@ -36,7 +36,7 @@ class Fylr:
     def get_object_by_id(self, object_type, id):
         get_url = join(
             self.db_url,
-            f'{object_type}/_all_fields/',
+            f'{object_type}/{object_type}__servicer',
             str(id)
         )
         params = {
@@ -92,7 +92,7 @@ class Fylr:
         params = { 'access_token': self.access_token }
         data = {
             '_objecttype': object_type,
-            '_mask': '_all_fields'
+            '_mask': object_type + '__servicer'
         }
         if pool is not None:
             fields_data['_pool'] = pool
